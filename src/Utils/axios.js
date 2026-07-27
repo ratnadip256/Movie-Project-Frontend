a80db62f90e2e2326d64e6df8f2bce47e778139d
Ratnadip <ratnadipshit44@gmail.com>
2026-07-28 01:50:17 +0530

    Update axios configuration

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1/users';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://movie-project-backend-beige.vercel.app/api/v1/users';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
